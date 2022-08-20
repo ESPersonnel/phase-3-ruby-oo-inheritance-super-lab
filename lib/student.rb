81e4a232657dd052cb5d5a89f3aa29d2303f5fec
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-20 21:52:18 +0300

    commit tree 50c8d9b11005f31ea2463ef3a251747e5421868b

---
+++ lib/student.rb
@@ -0,0 +1,11 @@
+
+require_relative './student.rb'
+
+class Student
+    def hello
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+    def raise_hand
+        puts "Pick me!"
+    end
+end
